1974343961a1a9c95a693231e4878e965a712717
yuwinzer <yu.moskalev@gmail.com>
2021-09-19 22:17:10 +0300

    added 3 task solutions

---
+++ lesson_4/2_task.js
@@ -0,0 +1,23 @@
+﻿class item {
+    constructor (name, price) {
+        this.name = name;
+        this.price = price;
+    };
+};
+
+let itemId1 = new item('chair1', 100);
+let itemId2 = new item('chair2', 300);
+let itemId3 = new item('sofa1', 300);
+
+function countCartPrice(x) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+    return x.reduce(reducer, 0); // 0 - is initial Value
+};
+
+let cart = [
+    itemId1,
+    itemId2,
+    itemId3
+];
+
+console.log(countCartPrice(cart));
