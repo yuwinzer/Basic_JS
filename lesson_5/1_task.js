0787e79674e4285cfb1eaa0bc4ec93d41d540a73
yuwinzer <yu.moskalev@gmail.com>
2021-09-25 01:52:29 +0300

    added 1 and 2 tasks

--- lesson_5/1_task.js
+++ lesson_5/1_task.js
@@ -1,1 +1,32 @@
-﻿
+﻿function tableCreate(x,y,cellSize) {
+    let tempTable = document.createElement('table');
+    tempTable.style.border = "0px solid black";
+    tempTable.style.borderCollapse = "collapse";
+    for (let i = 0; i < x; i++) {
+        let tableTr = tempTable.insertRow();
+        for(let j = 0; j < y; j++) {
+            var tableTd = tableTr.insertCell();
+            tableTd.style.border = "0px solid black";
+            tableTd.style.textAlign = 'center';
+            tableTd.style.backgroundColor = '#dab78d';
+            tableTd.style.width = cellSize + 'px';
+            tableTd.style.height = cellSize + 'px';
+
+            if ((i % 2 == 0 && j % 2 != 0 && i != 0 && j != 0) || (i % 2 != 0 && j % 2 == 0 && i != 0 && j != 0)) {
+                tableTd.style.backgroundColor = '#bb7c46';
+                tableTd.style.border = "1px solid black";
+            } else if (i != 0 && j != 0) {
+                tableTd.style.border = "1px solid black";
+            } else if (i == 0 && j != 0) {
+                tableTd.appendChild(document.createTextNode(String.fromCharCode(64+j)));
+                tableTd.style.backgroundColor = 'white';
+            } else if (i != 0 && j == 0) {
+                tableTd.appendChild(document.createTextNode(i));
+                tableTd.style.backgroundColor = 'white';
+            } else tableTd.style.backgroundColor = 'white';
+        };
+    };
+    document.body.appendChild(tempTable);
+};
+
+tableCreate(9, 9, 20);
--- lesson_5/2_task.js
+++ lesson_5/2_task.js
@@ -1,1 +1,64 @@
-﻿
+﻿class product {
+    constructor (id, name, color, cushion, size, price) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.cushion = cushion;
+        this.size = size;
+        this.price = price
+    };
+};
+
+
+class cart_item {
+    constructor (x) {
+        this.name = x.name;
+        this.price = x.price;
+    };
+};
+
+
+class cart {
+    constructor (items) {
+        this.items = items;
+    };
+
+    countCartPrice() {
+        if (this.items != null) {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+        let count = this.items.length;
+        return "Всего товаров: " + count + ", на сумму " + this.items.reduce(reducer, 0) + " баксов"; // 0 - is initial Value
+        } else {return "Корзина пустая"};
+    };
+};
+
+
+const chair1 = new product(135, 'Chair 1', 'Gold', 'Nuri', '1/4 scale', 86);
+const chair2 = new product(25, 'Chair 2', 'Black', 'Velvet black', '1/4 scale', 100);
+const vaseStand1 = new product(135, 'Vase stand 1', 'Gold', null, '1/4 scale', 100);
+
+
+const itemId1 = new cart_item(chair1);
+const itemId2 = new cart_item(chair2);
+const itemId3 = new cart_item(vaseStand1);
+
+
+const cart1 = new cart([itemId1, 
+                        itemId2,
+                        itemId3]);
+
+
+const cart2 = new cart();
+
+
+let cartContent = document.getElementById('cart_div');
+
+// filled cart:
+console.log(cart1.countCartPrice());
+cartContent.innerHTML = cart1.countCartPrice();
+
+//empty cart:
+console.log(cart2.countCartPrice());
+// cartContent.innerHTML = cart2.countCartPrice(); 
+
+
